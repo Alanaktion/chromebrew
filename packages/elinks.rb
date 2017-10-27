1576d6d4cf1245546bc8138de30e950712bc5854
Alan Hardman <alanh@sent.com>
2017-10-26 23:15:31 -0600

    Switching elinks to use precompiled source files

--- packages/elinks.rb
+++ packages/elinks.rb
@@ -3,26 +3,29 @@ require 'package'
 class Elinks < Package
   description 'Full-Featured Text WWW Browser'
   homepage 'http://elinks.or.cz/'
-  version '0.11.7'
-  source_url 'http://elinks.or.cz/download/elinks-0.11.7.tar.gz'
-  source_sha256 '94dc254cdecddee51819ae17962d9e606690bee3ec43fb43c635cb40197b4bc7'
+  version '0.13-19'
 
-  binary_url ({
-  })
-  binary_sha256 ({
-  })
+  case ARCH
+  when 'aarch64'
+    source_url 'https://github.com/Alanaktion/chromebrew/releases/download/elinks-v0.13-19/elinks-0.13-19-armv7h.pkg.tar.gz'
+    source_sha256 '2cdf3cd08402bf1ecf6d7ae6596845368057f0d432d41a8aca6b52c1fa77e539'
+  when 'armv7l'
+    source_url 'https://github.com/Alanaktion/chromebrew/releases/download/elinks-v0.13-19/elinks-0.13-19-armv7h.pkg.tar.gz'
+    source_sha256 '2cdf3cd08402bf1ecf6d7ae6596845368057f0d432d41a8aca6b52c1fa77e539'
+  when 'i686'
+    source_url 'https://github.com/Alanaktion/chromebrew/releases/download/elinks-v0.13-19/elinks-0.13-19-i686.pkg.tar.gz'
+    source_sha256 '300e3d1e27b81d2d692e140c90ba53c289512b0c985524758dddb490209fe0f5'
+  when 'x86_64'
+    source_url 'https://github.com/Alanaktion/chromebrew/releases/download/elinks-v0.13-19/elinks-0.13-19-x86_64.pkg.tar.gz'
+    source_sha256 '4b4ecdefadf5ac11a04d871a65d68aae107c8b07bada32300ded3f5195233e05'
+  end
 
   depends_on 'bz2'
   depends_on 'lzma'
   depends_on 'openssl'
   depends_on 'zlibpkg'
 
-  def self.build
-    system "./configure --prefix=#{CREW_PREFIX}"
-    system "make"
-  end
-
   def self.install
-    system "make", "DESTDIR=#{CREW_DEST_DIR}", "install"
+    system "install -Dm755 elinks #{CREW_DEST_PREFIX}/bin/elinks"
   end
 end
